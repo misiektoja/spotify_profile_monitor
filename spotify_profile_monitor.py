a7a855d981d4c6ce31635036e05a4568164a2d72
misiektoja <misiektoja-github@rm-rf.ninja>
2026-01-31 14:16:57 +0100

    improve(logging): expand tabs to spaces in log files for consistent alignment

--- spotify_profile_monitor.py
+++ spotify_profile_monitor.py
@@ -785,7 +785,9 @@ class Logger(object):
         self.logfile = open(filename, "a", buffering=1, encoding="utf-8")
 
     def write(self, message):
-        self.logfile.write(message)
+        # Expand tabs for file output (stdout remains untouched)
+        expanded_message = message.expandtabs(8)
+        self.logfile.write(expanded_message)
         if (TRUNCATE_CHARS):
             message = truncate_string_per_line(message, TRUNCATE_CHARS)
         self.terminal.write(message)
